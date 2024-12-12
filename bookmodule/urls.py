d0200ca8a1bcf1f2ecc8368d35cec9e1570bd4ef
talakutt <kimy1395@gmail.com>
2024-12-12 17:16:18 +0300

    lab3

--- bookmodule/urls.py
+++ bookmodule/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.start)
+    # path('', views.start)
+    path('', views.index),
+path('index2/<int:val1>/', views.index2),
+path('<int:bookId>', views.viewbook)
 ]
