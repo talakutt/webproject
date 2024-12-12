fe25cb19a95b99bd6c14f5d43f9284f68c1a01e0
talakutt <kimy1395@gmail.com>
2024-12-12 17:42:56 +0300

    new

--- bookmodule/urls.py
+++ bookmodule/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # path('', views.start)
     path('', views.index),
 path('index2/<int:val1>/', views.index2),
-path('<int:bookId>', views.viewbook)
+path('<int:bookId>', views.viewbook),
+path('search/', views.search)
 ]
